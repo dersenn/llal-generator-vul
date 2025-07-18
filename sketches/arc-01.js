bbbc4f90b2278dad5ef193b151a7ead789c29ce9
Christoph Senn <67318888+dersenn@users.noreply.github.com>
2025-07-18 19:56:43 +0200

    values and more sketches.

--- sketches/arc-01.js
+++ sketches/arc-01.js
@@ -79,7 +79,7 @@ class ArcSketch {
       useNoise: true,
       borderTop: 0,
       wdths: [50, 100, 150, 200],
-      opacityLevels: [5, 25, 50, 75, 100],
+      opacityLevels: [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100],
       nCols: 20,
       leftAngle: 24,
       rightAngle: 24,
